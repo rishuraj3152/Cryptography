d84b60fef0b8b04938adac87e802df4f725b09cf
rishuraj3152 <45044097+rishuraj3152@users.noreply.github.com>
2019-06-23 12:23:56 +0530

    Add files via upload

---
+++ extended euclidean algo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int gcdExtended(int a, int b, int *x, int *y)
+{
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+
+    int x1, y1;
+    int gcd = gcdExtended(b%a, a, &x1, &y1);
+
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+
+    return gcd;
+}
+
+int main()
+{
+    int x,y,a,b;
+    printf("enter the two numbers:");
+    scanf("%d%d",&a,&b);
+    int g = gcdExtended(a, b, &x, &y);
+    printf("gcd(%d, %d) = %d\n", a, b, g);
+    printf("The extended euclidean form is:%d*(%d)+%d*(%d)=%d",a,x,b,y,g);
+    return 0;
+}
